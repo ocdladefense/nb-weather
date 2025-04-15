a1b6791f0a1f8004a5150b900123402b0e7fc068
Orion Hill <Orion123333@gmail.com>
2025-04-15 13:53:32 -0700

    removed async keyword

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -3,10 +3,14 @@ import WeatherParser from "./WeatherParser.js";
 const apikey = process.env.API_KEY;
 
 // Helper functions to build URLs
+
 const buildGeoUrl = (zip) => `http://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=${apikey}`;
 const buildWeatherUrl = (lat, lon) => `https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${lat}&lon=${lon}&appid=${apikey}`;
 
 export default class Controller {
+    buildQueryString() {
+
+    }
     async fetchLatLon(zip) {
         try {
             const response = await fetch(buildGeoUrl(zip));
@@ -29,17 +33,25 @@ export default class Controller {
         }
     }
 
-    async sendRawWeatherDataToParser(weatherData) {
-        try {
-            let parser = new WeatherParser(weatherData.list);
-
-            return parser.finalArray;
-
-        } catch (err) {
-            console.error("Error in fetchForecast:", err);
-            throw err;
-
-
+    sendRawWeatherDataToParser(weatherData) {
+        let err = "";
+        if (!weatherData.list) {
+            err = "No forecast available."
+            console.log(err);
         }
+
+        if (!weatherData)
+
+            try {
+                let parser = new WeatherParser(weatherData.list);
+
+                return parser.finalArray;
+
+            } catch (err) {
+                console.error("Error in fetchForecast:", err);
+                throw err;
+
+
+            }
     }
 }
